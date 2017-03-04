8d00f4a4b57dc24a2aaa4503f51f54d58e65b13b
gan <Password1$>
2017-03-03 21:40:17 -0500

    run time permission

--- app/src/main/java/com/example/gan/testtestrun/MyPreferencesActivity.java
+++ app/src/main/java/com/example/gan/testtestrun/MyPreferencesActivity.java
@@ -14,6 +14,10 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.List;
@@ -23,14 +27,17 @@ import java.util.logging.Handler;
  * Created by gan on 2/28/17.
  */
 
-public class MyPreferencesActivity extends PreferenceActivity {
+public class MyPreferencesActivity extends PreferenceActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
     private static final int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 100;
+    private static final int PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION = 200;
     private WifiManager wifiManager;
     private List<ScanResult> wifiList;
+    private View rootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        rootView = findViewById(android.R.id.content);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferencesFragment()).commit();
         wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         if (!wifiManager.isWifiEnabled()) {
@@ -38,7 +45,19 @@ public class MyPreferencesActivity extends PreferenceActivity {
         }
         WifiConnectionReceiver receiver = new WifiConnectionReceiver(new android.os.Handler());
         registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        wifiManager.startScan();
+        scanWifi();
+    }
+
+    private void scanWifi() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+            requestLocationPermission();
+        }
+        else {
+            wifiManager.startScan();
+        }
     }
 
     public static class MyPreferencesFragment extends PreferenceFragment {
@@ -52,10 +71,20 @@ public class MyPreferencesActivity extends PreferenceActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions,
                                            int[] grantResults) {
-        if (requestCode == PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            // Do something with granted permission
-            wifiList = wifiManager.getScanResults();
+        if (requestCode == PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION)
+        {
+            if(grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Snackbar.make(rootView, "Fine location permission granted", Snackbar.LENGTH_SHORT).show();
+                wifiManager.startScan();
+            }
+            else
+                Snackbar.make(rootView, "Fine location permission granted", Snackbar.LENGTH_SHORT).show();
+
+        }
+        else
+        {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
@@ -70,21 +99,9 @@ public class MyPreferencesActivity extends PreferenceActivity {
         public void onReceive(final Context context, Intent intent) {
             final StringBuilder sb = new StringBuilder();
 
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION);
-                //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
-
-            }else{
-                wifiList = wifiManager.getScanResults();
                 //do something, permission was previously granted; or legacy device
-            }
-
-
-
-
-            wifiList = wifiManager.getScanResults();
-            ListPreference wifiConnections = (ListPreference) findPreference("wifiConnections");
+                wifiList = wifiManager.getScanResults();
+                ListPreference wifiConnections = (ListPreference) findPreference("wifiConnections");
 
 //            for(int i = 0; i < wifiList.size(); i++){
 //
@@ -92,15 +109,35 @@ public class MyPreferencesActivity extends PreferenceActivity {
 //                sb.append((wifiList.get(i)).toString());
 //                sb.append("\n\n");
 //            }
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    for (int i = 0; i < wifiList.size(); i++) {
-                        Toast.makeText(context, wifiList.get(i).toString(), Toast.LENGTH_SHORT).show();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < wifiList.size(); i++) {
+                            Toast.makeText(context, wifiList.get(i).toString(), Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }
-            });
+                });
+            }
 
+    }
+
+    private void requestLocationPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION))
+        {
+            Snackbar.make(rootView, R.string.permission_fine_location_rationale, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok, new View.OnClickListener(){
+
+                        @Override
+                        public void onClick(View v) {
+                            ActivityCompat.requestPermissions(MyPreferencesActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION);
+                        }
+                    }).show();
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(MyPreferencesActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION);
         }
     }
 }
