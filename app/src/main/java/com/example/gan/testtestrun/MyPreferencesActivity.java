776b2df772bbc8a4309488332885998dca468d5b
gan <Password1$>
2017-03-04 10:02:22 -0500

    run time permission

--- app/src/main/java/com/example/gan/testtestrun/MyPreferencesActivity.java
+++ app/src/main/java/com/example/gan/testtestrun/MyPreferencesActivity.java
@@ -2,6 +2,8 @@ package com.example.gan.testtestrun;
 
 import android.Manifest;
 import android.animation.Animator;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +14,7 @@ import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.design.widget.Snackbar;
@@ -20,6 +23,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Handler;
 
@@ -33,19 +37,47 @@ public class MyPreferencesActivity extends PreferenceActivity implements Activit
     private WifiManager wifiManager;
     private List<ScanResult> wifiList;
     private View rootView;
+    private static ListPreference wifiConnections;
+    private static ProgressDialog waitDlg;
+    private static boolean scanComplete;
+    private WifiConnectionReceiver receiver;
 
+    public MyPreferencesActivity() {
+    }
+
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         rootView = findViewById(android.R.id.content);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferencesFragment()).commit();
+
+        waitDlg = new ProgressDialog(this);
+        waitDlg.setTitle("Wait...");
+        waitDlg.setMessage("Wait for wifi info");
+        waitDlg.setCancelable(false);
+        scanComplete = false;
+
         wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         if (!wifiManager.isWifiEnabled()) {
             wifiManager.setWifiEnabled(true);
         }
-        WifiConnectionReceiver receiver = new WifiConnectionReceiver(new android.os.Handler());
-        registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+//        receiver = new WifiConnectionReceiver(new android.os.Handler());
+//        registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         scanWifi();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        receiver = new WifiConnectionReceiver(new android.os.Handler());
+        registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 
     private void scanWifi() {
@@ -60,11 +92,61 @@ public class MyPreferencesActivity extends PreferenceActivity implements Activit
         }
     }
 
+    //public static class MyPreferencesFragment extends PreferenceFragment implements OnTaskCompleted {
     public static class MyPreferencesFragment extends PreferenceFragment {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+            wifiConnections = (ListPreference)findPreference("wifiConnections");
+            if(wifiConnections != null)
+            {
+                wifiConnections.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        scanComplete = false;
+                        waitDlg.show();
+
+                        new Thread(){
+                            public void run(){
+                                Activity parentActivity = getActivity();
+                                for(int i=0; i<=16; i++) {
+                                    if(scanComplete)
+                                    {
+                                        waitDlg.dismiss();
+                                        if(parentActivity != null) {
+                                            parentActivity.runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    wifiConnections.setSummary("Select your home wifi");
+                                                }
+                                            });
+                                        }
+                                        return;
+                                    }
+                                    try {
+                                        Thread.sleep(500);
+                                        i++;
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                waitDlg.dismiss();
+                                if(parentActivity != null) {
+                                    parentActivity.runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            wifiConnections.setSummary("No wifi network found");
+                                        }
+                                    });
+                                }
+
+                            }
+                        }.start();
+                        return false;
+                    }
+                });
+            }
         }
     }
 
@@ -79,7 +161,7 @@ public class MyPreferencesActivity extends PreferenceActivity implements Activit
                 wifiManager.startScan();
             }
             else
-                Snackbar.make(rootView, "Fine location permission granted", Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(rootView, "Fine location permission denied", Snackbar.LENGTH_SHORT).show();
 
         }
         else
@@ -95,26 +177,40 @@ public class MyPreferencesActivity extends PreferenceActivity implements Activit
             this.handler = handlerIn;
         }
 
+        @SuppressWarnings("deprecation")
         @Override
         public void onReceive(final Context context, Intent intent) {
             final StringBuilder sb = new StringBuilder();
 
                 //do something, permission was previously granted; or legacy device
                 wifiList = wifiManager.getScanResults();
-                ListPreference wifiConnections = (ListPreference) findPreference("wifiConnections");
+                final ListPreference wifiConnections2 = (ListPreference) findPreference("wifiConnections");
 
-//            for(int i = 0; i < wifiList.size(); i++){
-//
-//                sb.append(new Integer(i+1).toString() + ". ");
-//                sb.append((wifiList.get(i)).toString());
-//                sb.append("\n\n");
-//            }
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
+
+                        final List<CharSequence> entries = new ArrayList<CharSequence>();
+                        final List<CharSequence> entryValues = new ArrayList<CharSequence>();
                         for (int i = 0; i < wifiList.size(); i++) {
-                            Toast.makeText(context, wifiList.get(i).toString(), Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(context, wifiList.get(i).toString(), Toast.LENGTH_SHORT).show();
+                            String ssid = wifiList.get(i).SSID;
+                            entries.add(ssid);
+                            entryValues.add(ssid);
                         }
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                wifiConnections.setEntries((CharSequence[])entries.toArray(new String[0]));
+                                wifiConnections.setEntryValues((CharSequence[])entryValues.toArray(new String[0]));
+                                scanComplete = true;
+                            }
+                        });
+//                        wifiConnections.setEntries((CharSequence[])entries.toArray(new String[0]));
+//                        wifiConnections.setEntryValues((CharSequence[])entryValues.toArray(new String[0]));
+//                        scanComplete = true;
+
                     }
                 });
             }
---
+++ app/src/main/java/com/example/gan/testtestrun/OnTaskCompleted.java
@@ -0,0 +1,5 @@
+package com.example.gan.testtestrun;
+
+public interface OnTaskCompleted{
+    void OnTaskCompleted();
+}
