d4ddefc941e404c5228dd50da784c1c4a9c0044d
gan <Password1$>
2017-03-19 21:21:23 -0400

    jobscheduler

--- app/src/main/java/com/example/gan/testtestrun/MainActivity.java
+++ app/src/main/java/com/example/gan/testtestrun/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gan.testtestrun;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +13,7 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.content.LocalBroadcastManager;
@@ -75,7 +79,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
         btnStart.setOnClickListener(this);
         btnStop.setOnClickListener(this);
-        if(HelperUtility.isServiceRunning(this, WifiService.class)){
+        if(HelperUtility.isServiceRunning(this, WifiJobService.class)){
             btnStart.setEnabled(false);
             btnStop.setEnabled(true);
         }
@@ -88,7 +92,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void onResume() {
         super.onResume();
-        IntentFilter intentFilter = new IntentFilter(WifiService.ACTION_WIFI);
+        IntentFilter intentFilter = new IntentFilter(WifiJobService.ACTION_WIFI);
         // use LocalBroadcastManager is performance is much better than global register
         //registerReceiver(receiver, intentFilter);
         LocalBroadcastManager.getInstance(this).registerReceiver(receiver, intentFilter);
@@ -111,27 +115,55 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnStart:
-                Intent intent = new Intent(getBaseContext(), WifiService.class);
-                intent.setAction(WifiService.ACTION_WIFI);
-                startService(intent);
+//                Intent intent = new Intent(getBaseContext(), WifiJobService.class);
+//                intent.setAction(WifiJobService.ACTION_WIFI);
+//                startService(intent);
+                scheduleJobs();
 
                 btnStart.setEnabled(false);
                 btnStop.setEnabled(true);
                 break;
             case R.id.btnStop:
-                stopService(new Intent(getBaseContext(), WifiService.class));
+                stopService(new Intent(getBaseContext(), WifiJobService.class));
                 btnStart.setEnabled(true);
                 btnStop.setEnabled(false);
+                cancelJobs();
                 break;
             default:
                 break;
         }
     }
 
+    private void scheduleJobs()
+    {
+        final long REFRESH_INTERVAL = 10000;
+        ComponentName serviceComponent = new ComponentName(this, WifiJobService.class);
+        JobInfo.Builder builder = new JobInfo.Builder(WifiJobService.JOB_ID, serviceComponent);
+        builder.setMinimumLatency(1 * 1000); // wait at least
+        builder.setOverrideDeadline(3 * 1000); // maximum delay
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED); // require unmetered network
+        //builder.setRequiresDeviceIdle(true); // device should be idle
+        builder.setRequiresCharging(false); // we don't care if the device is charging or not
+        builder.setPersisted(true);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//            builder.setMinimumLatency(REFRESH_INTERVAL);
+//        } else {
+//            builder.setPeriodic(REFRESH_INTERVAL);
+//        }
+        JobScheduler jobScheduler = (JobScheduler)getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.schedule(builder.build());
+    }
+
+    private void cancelJobs()
+    {
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.cancelAll();
+    }
+
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            String displayText = intent.getStringExtra(WifiService.PARAM_WIFI);
+            String displayText = intent.getStringExtra(WifiJobService.PARAM_WIFI);
             tvSignal.setText(displayText);
         }
     };
