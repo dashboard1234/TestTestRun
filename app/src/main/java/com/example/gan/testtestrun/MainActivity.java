8f28a402e46acc1991a33dab4dec68f5bed655d8
gan <Password1$>
2017-09-22 22:52:39 -0400

    use significant movement sensor to save power

--- app/src/main/java/com/example/gan/testtestrun/MainActivity.java
+++ app/src/main/java/com/example/gan/testtestrun/MainActivity.java
@@ -1,20 +1,13 @@
 package com.example.gan.testtestrun;
 
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.text.TextUtils;
 import android.util.Log;
@@ -22,10 +15,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Locale;
-
-import static com.example.gan.testtestrun.R.id.btnStart;
 
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
@@ -40,6 +29,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private Button btnStart;
     private Button btnStop;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,47 +39,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         btnStart = (Button)findViewById(R.id.btnStart);
         btnStop = (Button)findViewById(R.id.btnStop);
 
-
-//        SensorManager smg = (SensorManager) getSystemService(SENSOR_SERVICE);
-//        Sensor accelerometer = smg.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-//        int fifoSize = accelerometer.getFifoReservedEventCount();
-//        if (fifoSize > 0) {
-//            //Device supports batched sensor updates
-//            int i=0;
-//        }
-//
-//        Sensor movesensor = smg.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION);
-//        fifoSize = movesensor.getFifoReservedEventCount();
-//        if (fifoSize > 0) {
-//            //Device supports batched sensor updates
-//            int i=0;
-//        }
-
-        //wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
         SharedPreferences sharedPreferences = getSharedPreferences(WifiSettingActivity.WIFI_PREFERENCE, Context.MODE_PRIVATE);
         if(sharedPreferences != null)
             selectedWifi = sharedPreferences.getString(WifiSettingActivity.SELECTED_WIFI, "");
-
-//        speech=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-//
-//            @Override
-//            public void onInit(int status) {
-//                // TODO Auto-generated method stub
-//                if(status == TextToSpeech.SUCCESS){
-//                    int result=speech.setLanguage(Locale.CHINA);
-//                    if(result==TextToSpeech.LANG_MISSING_DATA ||
-//                            result==TextToSpeech.LANG_NOT_SUPPORTED){
-//                        Log.e("error", "This Language is not supported");
-//                    }
-//                    else{
-//                        //speech.speak("Master, please don't forget your keys", TextToSpeech.QUEUE_FLUSH, null);
-//                        //speech.speak("主人，别忘了带钥匙", TextToSpeech.QUEUE_FLUSH, null);
-//                    }
-//                }
-//                else
-//                    Log.e("error", "Initilization Failed!");
-//            }
-//        });
 
         btnStart.setOnClickListener(this);
         btnStop.setOnClickListener(this);
@@ -106,6 +58,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void onResume() {
         super.onResume();
+
         IntentFilter intentFilter = new IntentFilter(WifiJobService.ACTION_WIFI);
         // use LocalBroadcastManager is performance is much better than global register
         LocalBroadcastManager.getInstance(this).registerReceiver(receiver, intentFilter);
@@ -120,7 +73,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //speech.shutdown();
     }
 
     @Override
@@ -133,11 +85,13 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
                 btnStart.setEnabled(false);
                 btnStop.setEnabled(true);
+
                 break;
             case R.id.btnStop:
                 stopService(new Intent(getBaseContext(), WifiJobService.class));
                 btnStart.setEnabled(true);
                 btnStop.setEnabled(false);
+
                 break;
             default:
                 break;
